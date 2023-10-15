4d684d134559785bc32568d3dab2588321b2c15d
Lee <ehgus8621@gmail.com>
2023-10-15 21:08:01 +0900

    추천, 앵커, 마크다운, 검색

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pybo.apps.PyboConfig',
     'accounts.apps.AccountsConfig',
+
 ]
 
 MIDDLEWARE = [
---
+++ pybo/views/base_views.py
@@ -0,0 +1,55 @@
+from ..models import Question
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render
+from django.db.models import Q
+
+# Create your views here.
+def index(request):
+    """
+    pybo 목록출력
+    """
+    question_list = Question.objects.all().order_by('-create_date')
+    page = request.GET.get('page')
+    kw = request.GET.get('kw','')#검색어
+
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
+    paginator = Paginator(question_list, 10)
+    
+    
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        page_obj = paginator.page(page)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+        
+    leftIndex = (int(page) - 2)
+    if leftIndex < 1:
+        leftIndex = 1
+        
+    rightIndex = (int(page) + 2)
+    
+    if rightIndex > paginator.num_pages:
+        rightIndex = paginator.num_pages
+        
+    custom_range = range(leftIndex, rightIndex + 1)
+        
+    
+    context = {'question_list': question_list, 'page_obj': page_obj, 'paginator': paginator, 'custom_range': custom_range, 'kw': kw}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html',context)
