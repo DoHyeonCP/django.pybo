f1d81209129a4978359b92c515fff74bb4b65947
Lee <ehgus8621@gmail.com>
2023-11-26 15:33:20 +0900

    postgres 연동

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,3 +5,14 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 
 DEBUG = False
+
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'HOST': '13.125.16.6',
+        'PORT': '5432',
+        'NAME': 'pyko',
+        'USER': 'admin',
+        'PASSWORD': '1234'
+    }
+}
