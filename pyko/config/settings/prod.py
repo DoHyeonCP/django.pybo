79bc5c16bf9dfd5098889078d06cd6ba25a73d83
Lee <ehgus8621@gmail.com>
2023-11-29 06:17:52 +0900

    postgre 연동 전

--- pyko/config/settings/prod.py
+++ pyko/config/settings/prod.py
@@ -6,13 +6,13 @@ STATICFILES_DIRS = []
 
 DEBUG = False
 
-DATABASES = {
-    'default' : {
-        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'HOST': '13.125.16.6',
-        'PORT': '5432',
-        'NAME': 'pyko',
-        'USER': 'admin',
-        'PASSWORD': '1234'
-    }
-}
+# DATABASES = {
+#     'default' : {
+#         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+#         'HOST': '13.125.16.6',
+#         'PORT': '5432',
+#         'NAME': 'pyko',
+#         'USER': 'admin',
+#         'PASSWORD': '1234'
+#     }
+# }
