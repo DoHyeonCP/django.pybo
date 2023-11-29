5d43841aa7bdb61b228504a80b06fc9c5c6b9968
Lee <ehgus8621@gmail.com>
2023-11-29 20:28:43 +0900

    dockerfile 수정

--- pyko/config/settings/base.py
+++ pyko/config/settings/base.py
@@ -124,10 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/pyko/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    BASE_DIR / '/pyko/static'
+    BASE_DIR / '/static'
 ]
 
 MEDIA_URL = '/media/'
