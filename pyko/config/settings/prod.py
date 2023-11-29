e529946e0fba99b93989f6f87885c11321daac7b
Lee <ehgus8621@gmail.com>
2023-11-29 21:19:48 +0900

    수정

--- pyko/config/settings/prod.py
+++ pyko/config/settings/prod.py
@@ -1,9 +1,10 @@
 from .base import *
 
 ALLOWED_HOSTS = ['*']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/pyko/static/'
 STATICFILES_DIRS = []
 
+MEDIA_ROOT = '/pyko/media/'
 DEBUG = False
 
 # DATABASES = {
